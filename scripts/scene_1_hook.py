72e4d016fc9d8e521b2bd48125c41b69e18a1ff9
trieutienthanh284 <trieutienthanhmmd@gmail.com>
2026-04-07 21:35:09 +0700

    Update

--- scripts/scene_1_hook.py
+++ scripts/scene_1_hook.py
@@ -1,64 +1,48 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from skills.fami_lib import *
-from skills.fami_assets_helper import *
 
 class Scene1_Hook(FaMIBaseScene):
     def construct(self):
-        title = self.create_title("TIN NHẮN DỄ BỊ NHIỄU SÓNG?", "LÀM SAO ĐỂ ĐO LƯỜNG?")
+        title = self.create_title("CHẤT LƯỢNG ĐƯỜNG TRUYỀN")
         
         # Mobjects
-        tin_nhan = load_svg_icon("assets/Nhieu_Song/Hook/tin-nhan.svg")
-        if tin_nhan:
-            tin_nhan.set_stroke(width=4)
-            tin_nhan.scale(1.0).move_to(LEFT * 2.0 + UP * 0.5)
-        
-        may_bao = load_svg_icon("assets/Nhieu_Song/Hook/may-bao.svg")
-        if may_bao:
-            may_bao.set_stroke(width=4)
-            may_bao.scale(1.2).move_to(RIGHT * 1.5 + UP * 0.5)
-        
-        clock = load_svg_icon("assets/Nhieu_Song/Hook/clock.svg")
-        if clock:
-            clock.set_stroke(width=4)
-            clock.scale(1.5).move_to(UP * 0.5)
-        
+        tin_nhan = ImageMobject("assets/Bayes_Hiring/assets/icons/coding.png")
+        tin_nhan.scale_to_fit_width(2.0).move_to(UP * 1.5 + LEFT * 2.0)
+
+        van_de = ImageMobject("assets/Bayes_Hiring/assets/icons/problem.png")
+        van_de.scale_to_fit_width(2.0).move_to(UP * 1.5 + RIGHT * 2.0)
+
+        quality = ImageMobject("assets/Bayes_Hiring/assets/icons/quality.png")
+        quality.scale_to_fit_width(2.5).move_to(UP * -0.5)
+
         with self.voiceover(text="Tin nhắn gửi đi rất dễ bị nhiễu sóng.") as tracker:
             self.update_subtitle("Tin nhắn gửi đi rất dễ bị nhiễu sóng.")
             self.play(Write(title), run_time=min(1.0, tracker.duration * 0.3))
             
-            if tin_nhan:
-                anim, r_func = skill_pop_in(tin_nhan)
-                self.play(anim, rate_func=r_func, run_time=min(0.5, tracker.duration * 0.2))
+            anim, r_func = skill_pop_in(tin_nhan)
+            self.play(anim, rate_func=r_func, run_time=min(0.5, tracker.duration * 0.2))
             
-            if may_bao and tin_nhan:
-                self.play(
-                    FadeIn(may_bao, shift=DOWN), 
-                    Wiggle(tin_nhan, scale_value=1.2, rotation_angle=0.1 * PI),
-                    run_time=min(1.0, tracker.duration * 0.3)
-                )
+            self.play(
+                FadeIn(van_de, shift=DOWN), 
+                Wiggle(tin_nhan, scale_value=1.1, rotation_angle=0.08 * PI),
+                run_time=min(1.0, tracker.duration * 0.3)
+            )
             
         with self.voiceover(text="Vậy lấy gì để đo lường chất lượng đường truyền?") as tracker:
             self.update_subtitle("Vậy lấy gì để đo lường chất lượng đường truyền?")
             
-            anims_out = []
-            if tin_nhan: anims_out.append(FadeOut(tin_nhan, shift=LEFT))
-            if may_bao: anims_out.append(FadeOut(may_bao, shift=UP))
-            if anims_out:
-                self.play(*anims_out, run_time=min(0.5, tracker.duration * 0.2))
+            self.play(FadeOut(tin_nhan, shift=LEFT), FadeOut(van_de, shift=UP), run_time=min(0.5, tracker.duration * 0.2))
             
-            if clock:
-                anim, r_func = skill_pop_in(clock)
-                self.play(anim, rate_func=r_func, run_time=min(0.8, tracker.duration * 0.4))
-                
-                # Sóng tỏa ra
-                arcs = VGroup(*[
-                    Arc(radius=1.5 + i*0.6, angle=PI/2, start_angle=PI/4, color=SUCCESS, stroke_width=8)
-                    for i in range(3)
-                ])
-                arcs.rotate(PI/4)
-                arcs.next_to(clock, UP, buff=0.3)
-                
-                self.play(Create(arcs, lag_ratio=0.5), run_time=min(1.0, tracker.duration * 0.2))
+            anim, r_func = skill_pop_in(quality)
+            self.play(anim, rate_func=r_func, run_time=min(0.8, tracker.duration * 0.4))
+            
+            arcs = VGroup(*[
+                Arc(radius=1.2 + i*0.5, angle=PI/2, start_angle=PI/4, color=SUCCESS, stroke_width=8)
+                for i in range(3)
+            ])
+            arcs.next_to(quality, DOWN, buff=0.3)
+            
+            self.play(Create(arcs, lag_ratio=0.5), run_time=min(1.0, tracker.duration * 0.2))
 
         self.finish_scene()
